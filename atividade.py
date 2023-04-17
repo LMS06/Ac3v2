bfa9ebfa3084b6e7ccef11fbbbec82c3a8910d78
Luan Moura Sant'ana <2200960@SALAS.AULAS>
2023-04-17 20:40:46 -0300

    first commit

---
+++ atividade.py
@@ -0,0 +1,13 @@
+INICIO = int(input())
+FIM = int(input())
+primo= 0
+
+for i  in range (INICIO,FIM+1):
+    if i > 1:
+        for x in range(2,i):
+            if (i % x ==0 ):
+                break
+        else:
+            print(i,',',end="")
+            primo +=1
+print(f'primos:{primo}')
